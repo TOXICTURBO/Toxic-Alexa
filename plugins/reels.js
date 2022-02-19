46937d59ec943500eb15dee787bd5b51a3854b2a
TOXICTURBO <97358745+TOXICTURBO@users.noreply.github.com>
2022-02-19 21:42:26 +0530

    Update reels.js

--- plugins/reels.js
+++ plugins/reels.js
@@ -17,7 +17,7 @@ Asena.addCommand({pattern: 'insta ?(.*)', fromMe: false, desc: "Downloads from i
     var reply = await message.client.sendMessage(message.jid, LOAD_ING , MessageType.text, { quoted: message.data });
     
    const { status, type, data } = await instagram(match[1], 'julie')
-    if (!status) return await message.sendMessage('✅️Example:-\n\n*Change /reel/ to /p/ Then give Command .insta*\n\n*Any doubt ask to Nijin or Husni*')
+    if (!status) return await message.sendMessage('✅️Example:-\n\n*Change /reel/ to /p/ Then give Command .insta*\n\n*Any doubt ask to Turbo or Jifi*')
 
     reply = await message.client.sendMessage(message.jid,UPLOAD_ING , MessageType.text, { quoted: message.data });
     
