f5ede8b1d8e4b2804a3fdaa8e881612da5903e78
TOXICTURBO <97358745+TOXICTURBO@users.noreply.github.com>
2022-02-21 20:59:53 +0530

    Update setvar.js

--- plugins/setvar.js
+++ plugins/setvar.js
@@ -10,7 +10,7 @@ Asena.addCommand({pattern: 'setvar', fromMe: false, desc: Lang.WP}, (async (mess
     var r_text = new Array ();
     
     
-    r_text[0] = "https://i.ibb.co/3ypJ8QW/20211209-194953.jpg";
+    r_text[0] = "https://telegra.ph/file/f6ec3d3d845a1e3dcce39.jpg";
     
     
     var i = Math.floor(1*Math.random())
